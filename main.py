9a612e3bfe2b4eefb771682380e98976046aa4b2
Supreet Praharaj <supreet.praharaj.pro@gmail.com>
2024-04-01 11:58:24 +0530

    prepared base model

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from cnn_classifier import logger
 from cnn_classifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from cnn_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
-
-
-if __name__ == "__main__":
-  try:
+try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-  except Exception as e:
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+logger.info("****************************************************************")
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
     logger.exception(e)
     raise e
--- renalhealth_ai/cnn_classifier/entity/config_entity.py
+++ renalhealth_ai/cnn_classifier/entity/config_entity.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
 
 @dataclass
 class DataIngestionConfig:
@@ -10,3 +11,15 @@ class DataIngestionConfig:
   source_URL: str
   local_data_dir: Path
   unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+  root_dir: Path
+  base_model_path: Path
+  updated_base_model_path: Path
+  params_image_size: List[int]
+  params_learning_rate: float
+  params_include_top: bool
+  params_weights: str
+  params_classes: int
